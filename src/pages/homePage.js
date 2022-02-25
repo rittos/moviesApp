27feb95fe02af592034e7e27399da7dfb137ff55
Ritto Poovathingal Thimothy <20096059@mail.wit.ie>
2022-02-25 15:52:24 +0000

    Add to favourites logic completed; Dummy favourites page.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -40,7 +40,12 @@ const MovieListPage = (props) => {
     if (type === "title") setTitleFilter(value);
     else setGenreFilter(value);
   };
-
+  const addToFavourites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favourite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -63,7 +68,7 @@ const MovieListPage = (props) => {
         <Header title={"Home Page"} />
       </Grid>
       <Grid item container spacing={5}>
-        <MovieList movies={displayedMovies}></MovieList>
+      <MovieList movies={displayedMovies} selectFavourite={addToFavourites} />
       </Grid>
     </Grid>
     <Fab
