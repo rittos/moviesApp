7d9bebad5a7aa35d89151d4e92ae477612a4e387
Ritto Poovathingal Thimothy <20096059@mail.wit.ie>
2022-03-03 18:33:10 +0000

    Change site header.

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader'
 
 const App = () => {
   return (
     <BrowserRouter>
+     <SiteHeader />      {/* New Header  */}
+        
       <ul>
         <li>
           <Link to="/">Home</Link>
