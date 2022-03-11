a189424a894836d1944a3b3e6b9d2046e0d67eef
Ritto Poovathingal Thimothy <20096059@mail.wit.ie>
2022-03-11 13:45:23 +0000

    Adding pagination to homepage

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -1,9 +1,9 @@
-export const getMovies = () => {
+export const getMovies = (page) => {
     return fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=${page}`
     )
-      .then(res => res.json())
-      .then(json => json.results);
+      .then(res => res.json());
+      // .then(json => json.results);
   };
   
   export const getMovie = id => {
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import { getMovies } from "../api/tmdb-api";
+// import Pagination from '@material-ui/lab/Pagination';
+// import axios from 'axios';
+import ReactPaginate from 'react-paginate';
+import './App.css'
 
 const HomePage = (props) => {
+  const [perPage, setPerPage] = useState();
+  const [page, setPage] = useState();
+  const [pages, setPages] = useState();
   const [movies, setMovies] = useState([]);
   const favourites = movies.filter(m => m.favourite)
   localStorage.setItem('favourites', JSON.stringify(favourites))
@@ -14,19 +21,48 @@ const HomePage = (props) => {
     setMovies(updatedMovies);
   };
 
+  const handlePageClick = (event) => {
+    let page = event.selected;
+    page = page +1 ;
+    setPage({page});
+    // alert(page);
+    getMovies(page).then(movies => {
+      setMovies(movies.results);
+    });
+   }
+
   useEffect(() => {
-    getMovies().then(movies => {
-      setMovies(movies);
+    setPage(1);
+    // setPages(10);
+    setPerPage(10);
+    getMovies(page).then(movies => {
+      setMovies(movies.results);
+      // alert(movies.data.total_pages);
+      setPages(movies.total_pages);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
+    
+    <div>
     <PageTemplate
       title='Discover Movies'
       movies={movies}
       selectFavourite={addToFavourites}
     />
+    {/* <div style={{ display: 'block', padding: 10, backgroundColor: '#75cda4', marginTop: 10 }}>
+      <Pagination count={10} />
+    </div> */}
+    <ReactPaginate
+      previousLabel={'prev'}
+      nextLabel={'next'}
+      pageCount={pages}
+      onPageChange={handlePageClick}
+      containerClassName={'pagination'}
+      activeClassName={'active'}
+/>
+    </div>
   );
 };
 export default HomePage;
